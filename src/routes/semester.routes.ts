761e01e3ea43efc40bdebbdd74b4f32f5f05f1a6
Rohit Kumar Sah <rohitkumar233179@gmail.com>
2025-08-19 21:51:46 +0530

    Refactored Role Permission for students in Division,Batch,Semester Routes

--- src/routes/semester.routes.ts
+++ src/routes/semester.routes.ts
@@ -6,10 +6,10 @@ const semesterRoutes=Router();
 
 
 semesterRoutes.post("/",authenticateJwt,requireRoles("ADMIN","SUPER_ADMIN"),createSemester);
-semesterRoutes.get("/all/:divisionId",authenticateJwt,requireRoles("ADMIN","SUPER_ADMIN","TEACHER"),getAllSemester);
+semesterRoutes.get("/all/:divisionId",authenticateJwt,getAllSemester);
 semesterRoutes.patch("/:semesterId",authenticateJwt,requireRoles("ADMIN","SUPER_ADMIN"),updateSemester)
 semesterRoutes.delete("/:semesterId",authenticateJwt,requireRoles("ADMIN","SUPER_ADMIN"),deleteSemester)
-semesterRoutes.get("/:semesterId",authenticateJwt,requireRoles("ADMIN","SUPER_ADMIN"),getSemesterDetails)
+semesterRoutes.get("/:semesterId",authenticateJwt,getSemesterDetails)
 
 
 export default semesterRoutes;
