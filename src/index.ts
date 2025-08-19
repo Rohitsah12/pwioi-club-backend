7a76c4495d17b67dd5f9a3f65c15c7dac2ed0d37
Rohit Kumar Sah <rohitkumar233179@gmail.com>
2025-08-20 01:35:32 +0530

    Added Teacher Course Routes for all ongoing course and complete course

--- src/index.ts
+++ src/index.ts
@@ -23,6 +23,7 @@ import studentAttendanceRoutes from "./routes/studentattendance.routes.js";
 import studentAcademicsRoutes from "./routes/studentAcademics.routes.js";
 import subjectRoutes from "./routes/subject.routes.js";
 import examRoutes from "./routes/exam.routes.js";
+import teacherCourseRoutes from "./routes/teacherCourse.routes.js";
 
 const app = express();
 const PORT = process.env.PORT ?? 8000;
@@ -55,6 +56,7 @@ app.use("/api/student-attendance",studentAttendanceRoutes)
 app.use("/api/student-academics",studentAcademicsRoutes)
 app.use("/api/subjects",subjectRoutes)
 app.use("/api/exams",examRoutes)
+app.use("/api/teacher-courses",teacherCourseRoutes)
 
 
 
