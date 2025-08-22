5ff46588b33e3e751f17092bf873151a9da13850
Rohit Kumar Sah <rohitkumar233179@gmail.com>
2025-08-22 11:57:34 +0530

    Added Get All Admin

--- src/routes/superadmin.routes.ts
+++ src/routes/superadmin.routes.ts
@@ -1,12 +1,15 @@
 import { Router } from "express";
 import {prisma} from '../db/prisma.js'
 import { RoleType } from "@prisma/client";
-import { createSuperAdmin, getAllSuperAdmin } from "../controllers/superadmin.controller.js";
+import { createSuperAdmin, getAllAdmin, getAllSuperAdmin } from "../controllers/superadmin.controller.js";
 import { authenticateJwt, requireRoles } from "../middlewares/authMiddleware.js";
 
 const superadminrouter=Router();
 
 superadminrouter.post('/create',authenticateJwt,requireRoles("SUPER_ADMIN"),createSuperAdmin)
 superadminrouter.get('/all',authenticateJwt,requireRoles("SUPER_ADMIN"),getAllSuperAdmin)
+
+
+superadminrouter.get("",authenticateJwt,requireRoles("ADMIN","SUPER_ADMIN","OPS","BATCHOPS"),getAllAdmin)
 
 export default superadminrouter;
